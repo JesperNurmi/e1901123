d74206341d823b2f46b20d32b48789bd6628023c
JesperNurmi <103249744+JesperNurmi@users.noreply.github.com>
2022-04-30 10:51:24 +0300

    Delete src directory

--- src/controllers/ObservationController.js
+++
@@ -1,6 +0,0 @@
-//luetaan json tiedosto muuttujaan birds:
-export function getBirds(){
-    fetch('http://localhost:8080/getBirds')
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
