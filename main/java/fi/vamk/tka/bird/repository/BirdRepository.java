479562c8bfa1139ef646028f4991bdc1f32603e2
JesperNurmi <103249744+JesperNurmi@users.noreply.github.com>
2022-04-30 10:51:09 +0300

    Delete main directory

--- main/java/fi/vamk/tka/bird/repository/BirdRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.vamk.tka.bird.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fi.vamk.tka.bird.entity.Bird;
-
-public interface BirdRepository extends CrudRepository<Bird, Integer>{
-
-}
--- main/java/fi/vamk/tka/bird/repository/ObservationRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.vamk.tka.bird.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fi.vamk.tka.bird.entity.Observation;
-
-public interface ObservationRepository extends CrudRepository<Observation, Integer>{
-
-}
